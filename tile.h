e1a8e5f6c4190b7cee7f917d22cb042044654302
Nathan Killeen <nathan@FoxholeMKII.(none)>
2010-04-21 14:11:15 -0500

    Pull resolved.

--- tile.h
+++ tile.h
@@ -1,13 +1,28 @@
+/******************************************************
+ * 
+ * 
+ * 
+ * ****************************************************/
+#ifndef _TILE_H_
+#define _TILE_H_
 
 
-typedef struct tile {
-	public:
-	int x,y;
-	bool isSoldid, isWall;
-	
+#include<vector>
+#include<string>
+#include"enums.h"
+
+using namespace std;
+
+class Tile
+{
 	private:
-	w,h;
-
+		vector<void*> whatsOnMe[]; // creatures stored in 0, items in 1, interactables in 2
+	
 	public:
-	morph(int type);
+		Tile(int, int);
+		Tile();
+		string tileType;
+		int type;
+		int addOn(int, void*); // used to add to the tile's list of what is on top of it	
 };
+#endif
